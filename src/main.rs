ef2f1de357f0869afe95410c2202cfb2603ebdbc
ferluci148 <trabajodam18@gmail.com>
2024-12-21 12:26:41 +0100

    servo4

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,31 @@
 #![no_std]
 #![no_main]
 
+use arduino_hal::prelude::*;
+use arduino_hal::entry;
 use panic_halt as _;
+use arduino_hal::simple_pwm::SimplePwm;
 
-#[arduino_hal::entry]
-
-
-
-fn main() ->! {
-
-    
+#[entry]
+fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
+    
+    // Configuramos el pin D9 (PWM) para controlar el servo
+    let mut pwm = SimplePwm::new(pins.d9.into_output(), dp.TC0);
+    pwm.set_prescale::<arduino_hal::simple_pwm::Prescale64>();
 
-    /*
-     * For examples (and inspiration), head to
-     *
-     *     https://github.com/Rahix/avr-hal/tree/main/examples
-     *
-     * NOTE: Not all examples were ported to all boards!  There is a good chance though, that code
-     * for a different board can be adapted for yours.  The Arduino Uno currently has the most
-     * examples available.
-     */
-
-    let mut led = pins.d13.into_output();
- 
     loop {
- 
-        led.toggle();
-        arduino_hal::delay_ms(10000);
+        // Gira el servo a 0 grados
+        pwm.set_duty(0);
+        arduino_hal::delay_ms(1000);
+
+        // Gira el servo a 90 grados (ajusta el valor según sea necesario)
+        pwm.set_duty(128); // Valor para girar a 90 grados
+        arduino_hal::delay_ms(1000);
+
+        // Gira el servo a 180 grados
+        pwm.set_duty(255);
+        arduino_hal::delay_ms(1000);
     }
 }
